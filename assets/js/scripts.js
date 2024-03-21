be2745078704a97193c1610ab0e2d2fc1db68592
concillor <164273443+concillor@users.noreply.github.com>
2024-03-20 21:31:16 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function () {
+    $('#modoOscuro').click(function () {
+        $('body').toggleClass('dark-mode');
+        $(this).toggleClass('active');
+        $('#skills').toggleClass('.habilidades-oscuro');
+        $('.nombre').toggleClass('form-dark');
+        $('.email').toggleClass('form-dark');
+        $('.mensaje').toggleClass('form-dark');
+    });
+    $('#navegacion').scroll( function(){
+        $('#navegacion').toggleClass('fixed-top');
+    });
+    $('#skills').mouseover(function () {
+        $('.front').animate({
+            width: '80%'
+        }, 1000);
+        $('.bootstrap').animate({
+            width: '80%'
+        }, 1000);
+        $('.figma').animate({
+            width: '80%'
+        }, 1000);
+        $('.jq').animate({
+            width: '60%'
+        }, 1000);
+        $('.php').animate({
+            width: '60%'
+        }, 1000);
+        $('.team').animate({
+            width: '80%'
+        }, 1000);
+    });
+
+    $('#subir').click(function () {
+        $('html').animate({
+            scrollTop: '0'
+        });
+    });
+    
+});
